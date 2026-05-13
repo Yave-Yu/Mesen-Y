d149e210d99634252069ff690dc9eaaeb05091b1
Yave Yu <54294476+Yave-Yu@users.noreply.github.com>
2026-05-13 22:22:43 +0800

    Updated files via upload

--- Core/SoundMixer.cpp
+++ Core/SoundMixer.cpp
@@ -263,7 +263,7 @@ int16_t SoundMixer::GetOutputVolume(bool forRightChannel)
 	
 	return (int16_t)(squareVolume + tndVolume +
 		GetChannelOutput(AudioChannel::FDS, forRightChannel) * 20 +
-		GetChannelOutput(AudioChannel::MMC5, forRightChannel) * 43 +
+		GetChannelOutput(AudioChannel::MMC5, forRightChannel) * 14 +
 		GetChannelOutput(AudioChannel::Namco163, forRightChannel) * 20 +
 		GetChannelOutput(AudioChannel::Sunsoft5B, forRightChannel) * 15 +
 		GetChannelOutput(AudioChannel::VRC6, forRightChannel) * 60 +
